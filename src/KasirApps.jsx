42f3ee7ffb03ca389865dd18bbd654bedb631d70
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-19 11:28:00 +0700

    first commit

---
+++ src/KasirApps.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Kasirapps extends Component {
+    render() {
+        return (
+            <div>
+                <h1>masuk kasir apps</h1>
+            </div>
+        );
+    }
+}
+
+export default Kasirapps;
